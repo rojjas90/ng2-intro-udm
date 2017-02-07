65de1d78ff99677b5798dc02a85c0cfcc66c6bfd
grojas <grojas@dwsoftware.mx>
2017-02-07 17:39:06 -0600

    22. Directives We've create author component and all its references, also we've create a new directive to add make grow our input

---
+++ app/course.service.ts
@@ -0,0 +1,9 @@
+export class CourseService {
+    getCourses(): string[] {
+        return [
+            "Courses 1",
+            "Courses 2",
+            "Courses 3"
+        ];
+    }
+}
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,22 +1,26 @@
 import { Component } from "angular2/core";
+import { CourseService } from "./course.service";
+import {AutoGrowDirective} from "./auto-grow.directives";
 
 @Component({
     selector: "courses",
     template: `
     <h2>Courses</h2>
     {{title}}
+    <input type="text" autoGrow />
     <ul>
-    <li *ngFor="#course of courses">
-    {{course}}
-    </li>
-    </ul>
-    `
+      <li *ngFor = "#course of courses" >
+        {{course}}
+      </li>
+    </ul>`,
+    providers: [CourseService],
+    directives: [AutoGrowDirective]
 })
 export class CoursesComponent {
     title = "The title of courses page";
-    courses = [
-        "Courses 1",
-        "Courses 2",
-        "Courses 3"
-    ];
+    courses;
+
+    constructor(courseService: CourseService) {
+        this.courses = courseService.getCourses();
+    }
 }
