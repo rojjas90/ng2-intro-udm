744debd2881744df251ef256ce46ed091c52f508
grojas <grojas@dwsoftware.mx>
2017-02-24 17:07:31 -0600

    66. Control and ControlGroup We've commented code in our app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from "angular2/core";
-import { CoursesComponent } from "./courses.component";
-import { AuthorComponent } from "./author.component";
-import { ExerciseComponent } from "./exercise.component";
-import { FavoriteComponent } from "./favorite.component";
-import { VoteComponent } from "./vote.component";
-import { TwitListComponent } from "./twit-list.component";
-import { SummaryPipe } from "./summary.pipe";
-import { BootstrapPanelComponent } from "./bootstrap.panel.component";
+// import { CoursesComponent } from "./courses.component";
+// import { AuthorComponent } from "./author.component";
+// import { ExerciseComponent } from "./exercise.component";
+// import { FavoriteComponent } from "./favorite.component";
+// import { VoteComponent } from "./vote.component";
+// import { TwitListComponent } from "./twit-list.component";
+// import { SummaryPipe } from "./summary.pipe";
+// import { BootstrapPanelComponent } from "./bootstrap.panel.component";
 import { ZippyComponent } from "./zippy.component";
 import { ContactFormComponent } from "./contact/contact-form.component";
 
@@ -14,221 +14,228 @@ import { ContactFormComponent } from "./contact/contact-form.component";
     selector: "my-app",
     template: `
     <h1>{{title}}</h1>
-    <!-- Property binding -->
-    <h2 [textContent]="title"></h2>
 
-    <!-- ways to rendering content -->
-    <img src="{{imageUrl}}" />
-    <img [src]="imageUrl" />
-    <img bind-src="imageUrl" />
+    <!--
+      <! -- Property binding - ->
+      <h2 [textContent]="title"></h2>
 
-    <courses></courses>
-    <authors></authors>
-    <i class="glyphicon glyphicon-star"></i>
-    <exercise [favorite]="post.favorite" (changeCustom)="onFavoriteChange($event)"></exercise>
-    <favorite [favorite]="likes.favorite" [counterLikes]="likes.totalLike"></favorite>
+      <! -- ways to rendering content - ->
+      <img src="{{imageUrl}}" />
+      <img [src]="imageUrl" />
+      <img bind-src="imageUrl" />
 
-    <!-- My solution -->
-    <!-- <votes [voteCount]="votes.voteCount" [myVote]="votes.myVote" (vote)="votation($event)"></votes>-->
+      <courses></courses>
+      <authors></authors>
+      <i class="glyphicon glyphicon-star"></i>
+      <exercise [favorite]="post.favorite" (changeCustom)="onFavoriteChange($event)"></exercise>
+      <favorite [favorite]="likes.favorite" [counterLikes]="likes.totalLike"></favorite>
 
-    <!-- Mosh solution -->
-    <votes
-    [voteCount]="votes.voteCount"
-    [myVote]="votes.myVote"
-    (vote)="onVote($event)"></votes>
+      <! -- My solution - ->
+      <! -- <votes [voteCount]="votes.voteCount" [myVote]="votes.myVote" (vote)="votation($event)"></votes>- ->
 
-    <br>
-    <br>
-    <twits></twits>
-    <br>
-    <br>
+      <! -- Mosh solution - ->
+      <votes
+      [voteCount]="votes.voteCount"
+      [myVote]="votes.myVote"
+      (vote)="onVote($event)"></votes>
 
-<!-- using *ngIf directive -->
-    <div *ngIf="courses.length > 0">
-      List of courses
-    </div>
-    <div *ngIf="courses.length === 0">
-      You don't have any courses yet.
-    </div>
-    <br>
-    <!-- using hidden property -->
-    <div [hidden]="courses.length === 0">
-      List of courses
-    </div>
-    <div [hidden]="courses.length > 0">
-      You don't have any courses yet.
-    </div>
+      <br>
+      <br>
+      <twits></twits>
+      <br>
+      <br>
 
-    <br>
-    <br>
+      <! -- using *ngIf directive - ->
+      <div *ngIf="courses.length > 0">
+        List of courses
+      </div>
+      <div *ngIf="courses.length === 0">
+        You don't have any courses yet.
+      </div>
+      <br>
+      <! -- using hidden property - ->
+      <div [hidden]="courses.length === 0">
+        List of courses
+      </div>
+      <div [hidden]="courses.length > 0">
+        You don't have any courses yet.
+      </div>
 
-    <!-- ngSwitch review -->
-    <ul class="nav nav-pills">
-         <li [class.active]="viewMode==='map'"><a (click)="viewMode = 'map'">Map view</a></li>
-         <li [class.active]="viewMode==='list'"><a (click)="viewMode = 'list'">List view</a></li>
-    </ul>
-    <div [ngSwitch]="viewMode">
-         <template [ngSwitchWhen]="'map'">Map view content</template>
-         <template [ngSwitchWhen]="'list'">List view content</template>
-         <template ngSwitchDefault>Default view</template>
-    </div>
+      <br>
+      <br>
 
-    <br>
-    <br>
-    <!-- ngFor review -->
-    <ul>
-         <li *ngFor="#course of courses1, #i = index">
-              {{i + 1}} - {{ course }}
-         </li>
-    </ul>
+      <! -- ngSwitch review - ->
+      <ul class="nav nav-pills">
+           <li [class.active]="viewMode==='map'"><a (click)="viewMode = 'map'">Map view</a></li>
+           <li [class.active]="viewMode==='list'"><a (click)="viewMode = 'list'">List view</a></li>
+      </ul>
+      <div [ngSwitch]="viewMode">
+           <template [ngSwitchWhen]="'map'">Map view content</template>
+           <template [ngSwitchWhen]="'list'">List view content</template>
+           <template ngSwitchDefault>Default view</template>
+      </div>
 
-    <br>
-    <br>
-    <!-- Pipes review -->
-    {{data.title | uppercase | lowercase}}
-    <br>
-    {{data.students | number }}
-    <br>
-    {{data.rating | number: '2.2-2' }}
-    <br>
-    {{data.price | currency:'MXN':true:'2.2-2'}}
-    <br>
-    {{data.releaseDate | date:'dd MM yyyy'}}
-    <br>
-    {{data | json}}
-    <br>
-    <br>
-    <br>
+      <br>
+      <br>
+      <! -- ngFor review - ->
+      <ul>
+           <li *ngFor="#course of courses1, #i = index">
+                {{i + 1}} - {{ course }}
+           </li>
+      </ul>
 
-    <!-- Custom pipe review -->
-    {{post1.title}}
-    <br>
-    {{post1.body |summary:10 }}
-    <br>
-    <br>
-    <br>
-    <!-- ngStyle review -->
-    <button
-    [style.backgroundColor]="canSave ? 'blue' : 'gray'"
-    [style.color]="canSave ? 'white' : 'black'"
-    [style.fontWeight]="canSave ? 'bold' : 'normal'"
-    >Submit 1</button>
+      <br>
+      <br>
+      <! -- Pipes review - ->
+      {{data.title | uppercase | lowercase}}
+      <br>
+      {{data.students | number }}
+      <br>
+      {{data.rating | number: '2.2-2' }}
+      <br>
+      {{data.price | currency:'MXN':true:'2.2-2'}}
+      <br>
+      {{data.releaseDate | date:'dd MM yyyy'}}
+      <br>
+      {{data | json}}
+      <br>
+      <br>
+      <br>
 
-    <!-- or -->
+      <! -- Custom pipe review - ->
+      {{post1.title}}
+      <br>
+      {{post1.body |summary:10 }}
+      <br>
+      <br>
+      <br>
+      <! -- ngStyle review - ->
+      <button
+      [style.backgroundColor]="canSave ? 'blue' : 'gray'"
+      [style.color]="canSave ? 'white' : 'black'"
+      [style.fontWeight]="canSave ? 'bold' : 'normal'"
+      >Submit 1</button>
 
-    <button
-      [ngStyle]  ="{
-        backgroundColor: canSave ? 'blue' : 'gray',
-        color: canSave ? 'white' : 'black',
-        fontWeight: canSave ? 'bold' : 'normal'
-      }"
-    >Submit 2</button>
+      <! -- or - ->
 
-    <br>
-    <br>
-    <br>
-    <!-- Elvis operator review -->
-    <ul>
-         <li>Title: {{task.title}}</li>
-         <!--<li *ngIf="task.assignee !== null">Assigned to: {{task.assignee.name}}</li> -->
-         <li>Assigned to: {{task.assignee?.name}}</li>
-    </ul>
+      <button
+        [ngStyle]  ="{
+          backgroundColor: canSave ? 'blue' : 'gray',
+          color: canSave ? 'white' : 'black',
+          fontWeight: canSave ? 'bold' : 'normal'
+        }"
+      >Submit 2</button>
 
-    <br>
-    <br>
-    <br>
+      <br>
+      <br>
+      <br>
+      <! -- Elvis operator review - ->
+      <ul>
+           <li>Title: {{task.title}}</li>
+           <! --<li *ngIf="task.assignee !== null">Assigned to: {{task.assignee.name}}</li> - ->
+           <li>Assigned to: {{task.assignee?.name}}</li>
+      </ul>
 
-    <bs-panel>
-      <div class="heading">The heading</div>
-      <div class="body">This is the body!</div>
-      <div class="body">More content</div>
-    </bs-panel>
+      <br>
+      <br>
+      <br>
+      <! -- ng-content review - ->
+      <bs-panel>
+        <div class="heading">The heading</div>
+        <div class="body">This is the body!</div>
+        <div class="body">More content</div>
+      </bs-panel>
 
-    <br>
-    <br>
-    <br>
-    <zippy *ngFor="#zippy of zippyData" [title]="zippy.title">
-    {{zippy.content}}
-    </zippy>
-    <br>
-    <br>
-    <br>
+      <br>
+      <br>
+      <br>
+      -->
+      <!-- Zippy exercise  -->
+      <zippy *ngFor="#zippy of zippyData" [title]="zippy.title">
+      {{zippy.content}}
+      </zippy>
+      <br>
+      <br>
+      <br>
+
+
     <contact-form></contact-form>
     `,
     directives: [
-        CoursesComponent,
-        AuthorComponent,
-        ExerciseComponent,
-        FavoriteComponent,
-        VoteComponent,
-        TwitListComponent,
-        BootstrapPanelComponent,
+        // CoursesComponent,
+        // AuthorComponent,
+        // ExerciseComponent,
+        // FavoriteComponent,
+        // VoteComponent,
+        // TwitListComponent,
+        // BootstrapPanelComponent,
         ZippyComponent,
         ContactFormComponent
-    ],
-    pipes: [
-        SummaryPipe
     ]
+    // ,
+    // pipes: [
+    //     SummaryPipe
+    // ]
 })
 export class AppComponent {
     title = "Angular App";
-    imageUrl = "http://lorempixel.com/400/200";
-
-    post = {
-        title: "favorite",
-        favorite: true
-    };
-
-    likes = {
-        title: "favorite",
-        favorite: true,
-        totalLike: 10
-    };
-
-    votes = {
-        voteCount: 10,
-        myVote: 0
-    };
-
-    // *ngIf
-    courses = [];
-
-    // [ngSwitch] directive
-    viewMode = "map";
-
-    // *ngFor
-    courses1 = [
-        "courses1",
-        "courses2",
-        "courses3"
-    ];
-
-    // Pipes; concept which is used to format data
-    data = {
-        title: "Angular",
-        rating: 3.437654,
-        students: 5434,
-        price: 99.34,
-        releaseDate: new Date(2017, 2, 4)
-    };
-
-    // Custom pipes
-    post1 = {
-        title: "Angular tutorial",
-        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    };
-
-    // ngStyle review
-    canSave = true;
-
-    // Elvis operator review
-    task = {
-        title: "Review application",
-        assignee: null
-        //  {
-        //     name: "Rojas"
-        // }
-    };
+    // imageUrl = "http://lorempixel.com/400/200";
+    //
+    // post = {
+    //     title: "favorite",
+    //     favorite: true
+    // };
+    //
+    // likes = {
+    //     title: "favorite",
+    //     favorite: true,
+    //     totalLike: 10
+    // };
+    //
+    // votes = {
+    //     voteCount: 10,
+    //     myVote: 0
+    // };
+    //
+    // // *ngIf
+    // courses = [];
+    //
+    // // [ngSwitch] directive
+    // viewMode = "map";
+    //
+    // // *ngFor
+    // courses1 = [
+    //     "courses1",
+    //     "courses2",
+    //     "courses3"
+    // ];
+    //
+    // // Pipes; concept which is used to format data
+    // data = {
+    //     title: "Angular",
+    //     rating: 3.437654,
+    //     students: 5434,
+    //     price: 99.34,
+    //     releaseDate: new Date(2017, 2, 4)
+    // };
+    //
+    // // Custom pipes
+    // post1 = {
+    //     title: "Angular tutorial",
+    //     body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    // };
+    //
+    // // ngStyle review
+    // canSave = true;
+    //
+    // // Elvis operator review
+    // task = {
+    //     title: "Review application",
+    //     assignee: null
+    //     //  {
+    //     //     name: "Rojas"
+    //     // }
+    // };
 
     // Exercise
     zippyData = [
@@ -242,18 +249,18 @@ export class AppComponent {
         }
     ];
 
-    onFavoriteChange($event) {
-        console.log($event);
-    }
-
-    // // My solutions
-    // votation($event) {
+    // onFavoriteChange($event) {
     //     console.log($event);
     // }
-
-    // Mosh solution
-    onVote($event) {
-        console.log($event);
-    }
+    //
+    // // // My solutions
+    // // votation($event) {
+    // //     console.log($event);
+    // // }
+    //
+    // // Mosh solution
+    // onVote($event) {
+    //     console.log($event);
+    // }
 
 }
--- app/contact/contact-form.component.js
+++ app/contact/contact-form.component.js
@@ -1,5 +1,6 @@
-System.register(["angular2/core"], function (exports_1, context_1) {
+System.register(["angular2/core"], function(exports_1, context_1) {
     "use strict";
+    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -9,30 +10,29 @@ System.register(["angular2/core"], function (exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var __moduleName = context_1 && context_1.id;
-    var core_1, ContactFormComponent;
+    var core_1;
+    var ContactFormComponent;
     return {
-        setters: [
+        setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            }
-        ],
-        execute: function () {
+            }],
+        execute: function() {
             ContactFormComponent = (function () {
                 function ContactFormComponent() {
                 }
                 ContactFormComponent.prototype.ngOnInit = function () { };
+                ContactFormComponent = __decorate([
+                    core_1.Component({
+                        selector: "contact-form",
+                        templateUrl: "./app/contact/contact-form.component.html",
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ContactFormComponent);
                 return ContactFormComponent;
             }());
-            ContactFormComponent = __decorate([
-                core_1.Component({
-                    selector: "contact-form",
-                    templateUrl: "./app/contact/contact-form.component.html",
-                }),
-                __metadata("design:paramtypes", [])
-            ], ContactFormComponent);
             exports_1("ContactFormComponent", ContactFormComponent);
         }
-    };
+    }
 });
 //# sourceMappingURL=contact-form.component.js.map
