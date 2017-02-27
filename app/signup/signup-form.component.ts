2a9d22d6485cbab52be601fa4a20a51a90225336
grojas <grojas@dwsoftware.mx>
2017-02-27 16:18:12 -0600

    78. Creating Controls Explicitly We need to tell to Angular that we've created this control group and its control explicitly, to do this we have to use [ngFormModel] directive and bind it to the property in the component that represent the form group e.g [ngFormModel]="form" and then we need to associate each input field with the control object ngControl="inputName" the name we set to ngControl exactly the same as we've declare in the class component, in other case it will throw an exception
    To apply validation to our input field is better use a temporary variable such as
     #variableName="ngForm"
     and then we can use it in our binding expression like:
     *ngIf="!inputName.valid"

--- app/signup/signup-form.component.ts
+++ app/signup/signup-form.component.ts
@@ -1,8 +1,17 @@
-import {Component} from "angular2/core";
+import { Component } from "angular2/core";
+import { ControlGroup, Control, Validators} from "angular2/common";
 
 @Component({
     selector: "signup-form",
     templateUrl: "./app/signup/signup-form.component.html"
 })
 export class SignUpFormComponent {
+    form = new ControlGroup({
+        username: new Control("", Validators.required),
+        password: new Control("", Validators.required)
+    });
+
+    signup() {
+        console.log(this.form.value);
+    }
 }
