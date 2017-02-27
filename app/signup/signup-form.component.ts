98b518ccf9a52cb72c6ee61bd8fc67d7e23e1003
grojas <grojas@dwsoftware.mx>
2017-02-27 16:36:23 -0600

    79. Using FormBuilder There is also a more compact way to build control and control group objects, we can use the FormBuilder class
    when we create control groups we don't need to initialize it as:
    key: new Control()
    
    instead we pass an array, where each  element in that array represents one of the arguments of the control class
    key: []
    
    with this way we can create our forms easily, cleaner and more compact, is better to use this option when we have a large form with many control groups and many controls

--- app/signup/signup-form.component.ts
+++ app/signup/signup-form.component.ts
@@ -1,15 +1,24 @@
 import { Component } from "angular2/core";
-import { ControlGroup, Control, Validators} from "angular2/common";
+import { ControlGroup, Control, Validators, FormBuilder} from "angular2/common";
 
 @Component({
     selector: "signup-form",
     templateUrl: "./app/signup/signup-form.component.html"
 })
 export class SignUpFormComponent {
-    form = new ControlGroup({
-        username: new Control("", Validators.required),
-        password: new Control("", Validators.required)
-    });
+    // form = new ControlGroup({
+    //     username: new Control("", Validators.required),
+    //     password: new Control("", Validators.required)
+    // });
+
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder) {
+        this.form = fb.group({
+            username: ["", Validators.required],
+            password: ["", Validators.required]
+        });
+    }
 
     signup() {
         console.log(this.form.value);
